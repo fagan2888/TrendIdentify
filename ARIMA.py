b9ed051f6c41c23572e569b0e7bec3e87d531ca0
zhaoliyuan <zhaoliyuan@licaimofang.com>
2019-11-27 11:45:45 +0800

    start

--- ARIMA.py
+++ ARIMA.py
@@ -1,2 +1,24 @@
 
 # ARIMA algorithm
+# written by zhaoliyuan
+
+import numpy as np
+import pandas as pd
+import
+
+
+class ARIMA():
+
+    def loadData():
+
+        return df
+
+    def calParams():
+
+        params = [p,q]
+        return params
+
+    def evaluate():
+
+
+if __name__ == '__main__':
--- zigzag.py
+++ zigzag.py
@@ -1,2 +1,28 @@
 
 # zigzag algorithm
+# created by zhaoliyuan
+
+import numpy as np
+import pandas as pd
+from optparse import OptionParser
+
+class zigzag():
+
+    def __init__(self, depth, backstep, deviation):
+        self.depth = depth
+        self.backstep = backstep
+        self.deviation = deviation
+
+    def handle():
+
+        return signal
+
+
+if __name__ == '__main__':
+  
+    opt = OptionParser()
+    opt.add_option()
+    opt.add_option()
+    options,args = opt.parse_args()
+    zigzag = zigzag(options.depth, options.backstep, options.deviation)
+    zigzag.handle()
