e6649102d9eb5f98ba099a7c0d3c018a9ec037bd
zhaoly12 <zhaoly12@163.com>
2019-12-04 14:02:02 +0800

    'new'

--- MBSS.py
+++ MBSS.py
@@ -6,11 +6,38 @@ import pandas as pd
 import numpy as np
 from config import *
 from sqlalchemy import *
+from WindPy import *
+from optparse import OptionParser
 
 class MBSS():
-
-    def handle():
-
+    
+    def __init__(self, code, sdate, edate):
+        
+        self.code = code
+        self.sdate = sdate
+        self.edate = edate
+    
+    def loadData(self):
+        
+        code = self.code
+        sdate = self.sdate
+        edate = self.edate
+        
+        w.start()
+        _,df = w.wsd(code, "dealnum,volume,amt", sdate, edate, usedf = True)
+        
+        return df
+    
+    def handle(self,df):
+        
+        MBSS = list()
+        for i in range(len(df)):
+            mbss = df['VOLUME'][i]/df['DEALNUM'][i]
+            MBSS.append(mbss)
+        df['MBSS'] = MBSS
+        out = df['MBSS']
+           
+        return out
 
 
 
@@ -31,6 +58,11 @@ if __name__ == '__main__':
 #    sql = select()
 #    sql = sql.where()
 #    df = pd.read_sql(sql,db)
-
-    MBSS = MBSS()
-    MBSS.handle()
+    opt = OptionParser()
+    opt.add_option('--code', help = 'stock code', default = '000001.SZ')
+    opt.add_option('--sdate', help = 'start date', default = '2019-11-03')
+    opt.add_option('--edate', help = 'end date', default = '2019-12-02')
+    options, args = opt.parse_args()
+    MBSS = MBSS(options.code, options.sdate, options.edate)
+    df = MBSS.loadData()
+    out = MBSS.handle(df)
